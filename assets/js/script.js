1f3f6bbfb38d6925058722206d05689036025d5b
richie nolan <richienolan23@gmail.com>
2023-09-08 16:49:37 +0000

    add consts and eventlistners to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
-document.addEventListener("DOMContentLoaded" function() {
-    let buttons = document.getElementsByTagName("button");
+const buttons = document.getElementsByTagName("button")
+const playerScore = document.getElementById("player-count")
+const computerScore = document.getElementById("computer-count")
+const playerChoice = document.getElementById("player-choice")
+const computerChoice = document.getElementById("computer-choice")
+const messages = document.getElementById("game-info")
+const choices = ["rock", "paper", "scissors"];
 
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            let playerChoice = this.getAttribute("data-choice");
-            startGame(playerChoice)
-        })
-    }
-})
+for (let button of buttons) {
+    button.addEventListener("click", function () {
+        let playerData = this.getAttribute("data-choice")
+    })
+}
 
 function startGame()
 
