46907c97e3a38420c22d55be88d174441a17f862
richie nolan <richienolan23@gmail.com>
2023-09-11 16:09:53 +0000

    add functions to generate result and increment scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,82 @@
 const buttons = document.querySelectorAll("button")
-const playerScore = document.getElementById("player-count")
-const computerScore = document.getElementById("computer-count")
 const playerChoiceBox = document.getElementById("player-choice")
 const computerChoiceBox = document.getElementById("computer-choice")
 const messages = document.getElementById("game-info")
 let playerChoice
+let computerChoice
+let result
+let playerScore
+let computerScore
 
 buttons.forEach(button => button.addEventListener('click', (e) => {
     playerChoice = e.target.id
     playerChoiceBox.innerText = playerChoice
+    computerInput()
+    generateResult()
 }))
 
 
 
 // function playerInput()
 
-// function computerInput()
+function computerInput() {
 
-// function gameInfo()
+    const randomNumber = Math.floor(Math.random() * 3) + 1
 
-// function incrementPlayerScore()
+    if (randomNumber === 1) {
+        computerChoice = "rock"
+    }
+    if (randomNumber === 2) {
+        computerChoice = "paper"
+    }
+    if (randomNumber === 3) {
+        computerChoice = "scissors"
+    }
 
-// function incrementComputerScore()
+    computerChoiceBox.innerHTML = computerChoice
+
+}
+
+function generateResult() {
+    if (computerChoice === playerChoice) {
+        result = "It's a draw"
+    }
+    if (computerChoice === "rock" && playerChoice === "paper") {
+        result = "The Player Wins!"
+        incrementPlayerScore()
+    }
+    if (computerChoice === "rock" && playerChoice === "scissors") {
+        result = "The Computer Wins!"
+        incrementComputerScore()
+    }
+    if (computerChoice === "scissors" && playerChoice === "rock") {
+        result = "The Player Wins!"
+        incrementPlayerScore()
+    }
+    if (computerChoice === "scissors" && playerChoice === "paper") {
+        result = "The Computer Wins!"
+        incrementComputerScore()
+    }
+    if (computerChoice === "paper" && playerChoice === "scissors") {
+        result = "The Player Wins!"
+        incrementPlayerScore()
+    }
+    if (computerChoice === "paper" && playerChoice === "rock") {
+        result = "The Computer Wins!"
+        incrementComputerScore()
+    }
+
+    messages.innerHTML = result
+
+
+}
+
+function incrementPlayerScore() {
+    playerScore = parseInt(document.getElementById("player-count").innerText)
+    document.getElementById("player-count").innerText = ++playerScore
+}
+
+function incrementComputerScore() {
+    computerScore = parseInt(document.getElementById("computer-count").innerText)
+    document.getElementById("computer-count").innerText = ++computerScore
+}
